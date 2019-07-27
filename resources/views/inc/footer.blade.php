1b4fc164b8572fd5d3814c4db64f6b477314cb0f
FIlimoni Naisua <filimonidud@gmail.com>
2019-07-28 10:15:45 +1200

    Add PagesController &  inc views, styles and images
     inc views are the views included in every view layout:
    Header
    Footer
    Navbar

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,50 @@
+<footer>
+    <table width="100%" border="0" cellpadding="0" cellspacing="0">
+        <tbody>
+            <tr>
+                <td valign="top" width="20">
+                    <!-- -->
+                </td>
+                <hr bgcolor="#004251">
+                </hr>
+                <td colspan="2" valign="top" bgcolor="#004251"></td>
+                <td valign="top" width="20">
+                    <!-- -->
+                </td>
+            </tr>
+            <tr>
+                <td valign="top" width="20">
+                    <!-- -->
+                </td>
+                <td colspan="3" valign="top">
+                    <table width="100%" border="0" cellspacing="0" cellpadding="7">
+                        <tbody>
+                            <tr>
+                                <td align="left" valign="top" width="117"><a href="https://www.usp.ac.fj/"
+                                        target="_blank"
+                                        onclick="javascript:urchinTracker ('/.external/http/www.usp.ac.fj/'); ">
+                                        <div id="btlogo">
+                                            <div class="logocover"></div>
+                                        </div>
+                                    </a>
+                                </td>
+                                <td align="left" valign="top"><b><span class="disclaimer"><a
+                                                href="https://www.usp.ac.fj/index.php?id=disclaimer_copyright"
+                                                onfocus="blurLink(this);">Disclaimer &amp;
+                                                Copyright</a></span></b>&nbsp;<b>l</b>&nbsp;<b><span
+                                            class="disclaimer"><a href="https://www.usp.ac.fj/index.php?id=contact"
+                                                onfocus="blurLink(this);">Contact
+                                                Us</a></span></b>&nbsp;&nbsp;<b>l</b>&nbsp;<br>© Copyright 1968 - 2019.
+                                    All Rights Reserved.</td>
+                                <td align="right" valign="top"><b>The University of the South Pacific</b><br>
+                                    Laucala Campus, Suva, Fiji<br>
+                                    Tel: +679 323 1000</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td valign="top" width="20"></td>
+            </tr>
+        </tbody>
+    </table>
+</footer>
