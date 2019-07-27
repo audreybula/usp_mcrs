e1b1d4accc11fc6cb63d5be14c0b2d9b80e71db3
FIlimoni Naisua <filimonidud@gmail.com>
2019-07-28 10:16:38 +1200

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,13 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| never return a view from your route
+| what you should do is create a controller set the route to go
+| a controller function  then return the view
+|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
 
 Auth::routes();
 
