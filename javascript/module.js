3c3095095bb60970dd83840abc9a1852cdcd1232
audreybula <46657101+audreybula@users.noreply.github.com>
2019-09-18 20:24:21 +1200

    modify javascript files

--- javascript/module.js
+++ javascript/module.js
@@ -2,14 +2,16 @@ require(['jquery'], function($)
 {
 	$(document).ready(function()
 	{
-		// Variables to store values for each element
+		// Variables to store values for form elements
 		var ccodefield = document.getElementsByName("coursecode")[0];
 		var cnamefield = document.getElementsByName("coursename")[0];
 		var cfacultyfield = document.getElementsByName("coursefaculty")[0];
-		var cschoolfield = document.getElementsByName("courseschool")[0]; 
-		var cnumshells = document.getElementsByName("courseshellnumber")[0];
+		var cschoolfield = document.getElementsByName("courseschool")[0];
 
-		//If Course Code is changed, change values of all other elements to match course code
+		// If Course Code is changed, change other fields
+		$("select[name='coursefaculty']").prop("disabled", true);
+		$("select[name='courseschool']").prop("disabled", true);
+
 		$("select[name='coursecode']").change(function()
 		{
 			cnamefield.value = ccodefield.value;
@@ -17,16 +19,12 @@ require(['jquery'], function($)
 			cschoolfield.value = ccodefield.value;
 		});
 
-		//If Course Name is changed, change values of all other elements to match course name
+		// If Course Name is changed, change other fields
 		$("select[name='coursename']").change(function()
 		{
 			ccodefield.value = cnamefield.value;
 			cfacultyfield.value = cnamefield.value;
 			cschoolfield.value = cnamefield.value;
 		});
-
-		//Disable Course School and Course Faculty
-		$("select[name='courseschool']").prop("disabled", true);
-		$("select[name='coursefaculty']").prop("disabled", true);
 	});
 });
