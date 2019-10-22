c3e693e236eb401c0e4261691d4f80d417467b87
FIlimoni Naisua <filimonidud@gmail.com>
2019-10-23 05:32:18 +1200

    add mrcs email library

---
+++ email_lib.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * @package    block_usp_mcrs
+ * @copyright   2019 IS314 Group 4 <you@example.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+/**
+ * @param $USER
+ * @param $requested_course
+ * @return  $success
+ */
+function email_request_details_to_requester($USER, $requested_course)
+{
+    $email_user = new stdClass();
+    $email_user->email = "moodletest679@gmail.com";
+    $email_user->id = -99;
+    $message = 'Course Full Name: '. $requested_course->fullname.
+        'Course Short Name: '. $requested_course->shortname;
+    $success = email_to_user($email_user, $USER, get_string('message_header', 'block_usp_mcrs'), $message);
+    return $success;
+}
