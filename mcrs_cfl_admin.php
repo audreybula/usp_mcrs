e01708db8a52274c8408c36f87c91dd4c0d96e0a
FIlimoni Naisua <filimonidud@gmail.com>
2019-10-25 12:16:48 +1200

    Update mcrs_cfl_admin.php

--- mcrs_cfl_admin.php
+++ mcrs_cfl_admin.php
@@ -24,7 +24,7 @@
  */
 
 require_once('../../config.php');
-require_once($CFG->dirroot.'/blocks/usp_mcrs/adlib.php');
+require_once($CFG->dirroot.'/blocks/usp_mcrs/adminlib.php');
 
 
 require_login();
