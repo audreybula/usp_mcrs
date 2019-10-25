9dbb1906aa1f61e4b8bd314dcf7f5b8f8a97c0df
audreybula <46657101+audreybula@users.noreply.github.com>
2019-10-25 20:06:31 +1200

    update plugin strings

--- lang/en/block_usp_mcrs.php
+++ lang/en/block_usp_mcrs.php
@@ -20,28 +20,19 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-// Strings for block.
-$string['backup_and_delete'] = 'Backup And Delete';
-$string['block_index'] = 'Backup';
-$string['block_delete'] = 'Delete';
-$string['block_pending'] = 'Pending';
-$string['block_config'] = 'Config';
+// Strings for block
+$string['pluginname'] = 'Moodle Course Request';
 $string['block_requestcourse'] = 'Make A Course Request';
 $string['block_mcrs_admin'] = 'Moodle Admin Home';
 $string['block_mcrs_cfl_admin'] = 'CFL Admin Home';
 $string['block_configemail'] = 'Modify Email Template';
-$string['block_failed'] = 'Failures';
-$string['block_large_backups'] = 'Large Backups';
-$string['backing_up'] = 'Backing Up';
-$string['usp_mcrs_settings'] = 'Backup and Delete Settings';
-$string['status_running'] = 'Running for {$a} minute(s)';
+$string['usp_mcrs_settings'] = 'USP MCRS Settings';
 $string['cron_backup_error'] = 'Error backing up {$a}';
 $string['status_not_running'] = 'Not Running';
-$string['cron_already_running'] = 'Skipping usp_mcrs, it has already been ' .
-    'running for {$a} minute(s)';
+$string['cron_already_running'] = 'Skipping usp_mcrs, it has already been ' . 'running for {$a} minute(s)';
 $string['backuptask'] = 'Backup job';
 
-//Strings for support staff
+// Strings for support staff
 $string['support_staff_name'] = 'Support Staff';
 $string['course_count'] ='Course Count';
 $string['school'] = 'School';
@@ -66,16 +57,6 @@ $string['blended'] = 'Blended';
 $string['choosecoursecode'] = 'Choose Course Code';
 $string['choosecoursename'] = 'Choose Course Name';
 $string['choosecourseschool'] = 'Course School';
-$string['courseyeargeneral'] = 'Enter year course is to be offered';
-$string['coursesemestergeneral'] = 'Enter semester course is to offered';
-$string['courseyearf2f'] = 'Enter year course is to be offered';
-$string['coursesemesterf2f'] = 'Enter semester course is to offered';
-$string['courseyearonline'] = 'Enter year course is to be offered';
-$string['coursesemesteronline'] = 'Enter semester course is to offered';
-$string['courseyearprint'] = 'Enter year course is to be offered';
-$string['coursesemesterprint'] = 'Enter semester course is to offered';
-$string['courseyearblended'] = 'Enter year course is to be offered';
-$string['coursesemesterblended'] = 'Enter semester course is to offered';
 $string['choosecourseshellgeneral'] = 'Choose Course Shell To Be Copied';
 $string['choosecourseshell1'] = 'Choose F2F Course Shell To Be Copied';
 $string['choosecourseshell2'] = 'Choose Online Course Shell To Be Copied';
@@ -83,35 +64,6 @@ $string['choosecourseshell3'] = 'Choose Print Course Shell To Be Copied';
 $string['choosecourseshell4'] = 'Choose Blended Course Shell To Be Copied';
 $string['additionalinfo'] = 'Additional Information or Comments';
 $string['submitbutton'] = 'Submit';
-
-// Stings shared by pages.
-$string['pluginname'] = 'Moodle Course Request';
-$string['blockname'] = 'Moodle Course Request';
-$string['need_permission'] = 'You do not have permission to view this page';
-$string['toggle_all'] = 'Select All/None';
-
-// Strings for index.php.
-$string['build_search'] = 'Build Search';
-$string['saved_searches'] = 'Saved Searches';
-$string['no_searches'] = 'There are no saved searches at this time';
-$string['match'] = 'Match';
-$string['of_these_constraints'] = 'of these constraints';
-$string['build_search_button'] = 'Build Search Query';
-$string['search_name'] = 'Search Name';
-$string['created_at'] = 'Created At';
-$string['run_query_button'] = 'Run Saved Query';
-$string['upload_a_file'] = 'Upload A File';
-$string['upload'] = 'Upload';
-$string['cancel'] = 'Cancel';
-$string['delete_queries_link'] = 'Select Saved Queries for Deletion';
-$string['course_id'] = 'Course ID #';
-$string['is'] = 'is';
-$string['is_not'] = 'is not';
-$string['contains'] = 'contains';
-$string['does_not_contain'] = 'does not contain';
-$string['name_missing'] = 'Please select a name for this query';
-$string['term_missing'] = 'Please select at least one search term for this constraint';
-$string['search_missing'] = 'Please select a saved search';
 
 // Admin Home
 $string['moodle_admin'] = 'Moodle Admin Home';
@@ -122,60 +74,20 @@ $string['requestername'] = 'Requester';
 $string['requestlecturer'] = 'Lecturer';
 $string['requeststatus'] = 'Status';
 
-// Strings for backup.php.
-$string['backup'] = 'Select Backup Courses';
-$string['backup_button'] = 'Backup Selected Courses';
+// Backup
+$string['backup'] = 'Backup';
 
-// Strings for results.php.
-$string['search_results'] = 'Search Results';
-$string['save_query'] = 'Save Query';
-$string['create_new_query'] = 'Create New Query';
-
-// Strings for delete.php.
-$string['delete'] = 'Delete?';
-$string['delete_header'] = 'Completed Backups';
-$string['deleted'] = 'Successfully deleted {$a}';
-$string['delete_error'] = ', but there may have been an error, please check';
-$string['none_completed'] = 'There are no completed backups at this time';
-$string['delete_button'] = 'Delete Selected Courses';
-
-// Strings for delete_queries.php.
-$string['delete_queries_header'] = 'Delete Saved Queries';
-$string['delete_queries_button'] = 'Delete Selected Queries';
-$string['delete_queries_success'] = '{$a} successful query deletion(s)';
-
-// Strings for send_job.php.
-$string['job_sent'] = 'Backup Job Sent';
-$string['job_sent_body'] = 'Your backup job will start during the next cron run. ' .
-    'You will receive an email when all backups are complete.';
-$string['already_successful'] = ' was not scheduled for backup because was ' .
-    'already successfully backed up but never deleted.';
-$string['already_scheduled'] = ' was not scheduled for backup because it is ' .
-    'already scheduled for backup.';
-$string['already_failed'] = ' was not scheduled for backup because it is an ' .
-    'unresolved failure. Please fix this.';
-
-// String for failed.php.
-$string['failed_header'] = 'Failed Backups';
-$string['none_failed'] = 'There are no failed backups at this time';
-$string['failed_button'] = 'Reschedule Selected Backups';
-$string['failed'] = 'Failed?';
-$string['statuses_updated'] = 'Selected courses have been rescheduled for backup';
-
-// Strings for settings.php.
+// Settings
 $string['config_path'] = 'Storage Path';
-$string['config_path_desc'] = 'Relative to {$a}, include the surrounding slashes.
-    Ensure that this directory is created and writable.';
+$string['config_path_desc'] = 'Relative to {$a}, include the surrounding slashes. Ensure that this directory is created and writable.';
 $string['config_pattern'] = 'Archive suffix';
 $string['config_pattern_desc'] = 'Data that will be appended onto backup names';
 $string['config_roles'] = 'Roles';
 $string['config_roles_desc'] = 'Roles to include when naming backup files';
 $string['config_size_limit'] = 'Size limit before warning';
 $string['config_size_limit_desc'] = 'In megabytes';
-$string['path_error'] = 'Error: Please ensure that the path you provided is a ' .
-    'writable directory';
-$string['sched_config'] = 'Access scheduled backup settings as
-    (' . $string['pluginname'] . ') uses these settings.';
+$string['path_error'] = 'Error: Please ensure that the path you provided is a ' . 'writable directory';
+$string['sched_config'] = 'Access scheduled backup settings as (' . $string['pluginname'] . ') uses these settings.';
 $string['here'] = 'here';
 $string['config_path_not_exists'] = 'The path you have entered does not exists.';
 $string['config_path_not_writable'] = 'The path you have entered is not writable.';
